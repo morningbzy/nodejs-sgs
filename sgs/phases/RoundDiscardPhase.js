34ba06d30385bdcbd97f6942ca9ef4c2ebf605ac
morningbzy <morningbzy@gmail.com>
2018-06-11 23:30:56 +0800

    基本的弃牌阶段

--- sgs/phases/RoundDiscardPhase.js
+++ sgs/phases/RoundDiscardPhase.js
@@ -1,4 +1,5 @@
 const C = require('../constants');
+const utils = require('../utils');
 const Phase = require('./phase');
 
 
@@ -10,5 +11,30 @@ module.exports = class extends Phase {
     static* start(game) {
         console.log('ROUND-DISCARD-PHASE');
         const u = game.roundOwner;
+
+        let discardCount = Math.max(u.cards.size - u.hp, 0);
+        while(discardCount > 0) {
+            let command = yield game.wait(u, {
+                validCmds: ['PLAY_CARD', 'CANCEL'],
+                validator: (command) => {
+                    if(command.cmd === 'CANCEL') {
+                        return true;
+                    }
+                    return command.params.length <= discardCount;
+                },
+            });
+
+            let discardCardPks = command.params;
+            if(command.cmd === 'CANCEL') {
+                // Random discard cards
+                discardCardPks = utils.shuffle(u.cards).map((c) => c.pk).slice(0, discardCount);
+            }
+
+            console.log(discardCardPks);
+
+            game.removeUserCards(u, discardCardPks);
+            game.discardCards(discardCardPks);
+            discardCount = Math.max(u.cards.size - u.hp, 0);
+        }
     }
 };
--- sgs/utils.js
+++ sgs/utils.js
@@ -2,7 +2,7 @@
 module.exports = {
     // Shuffle an Array
     shuffle(arr) {
-        let _arr = [].concat(arr);
+        let _arr = [].concat(Array.from(arr));
         for(let j, x, l = _arr.length; l; j = parseInt(Math.random() * l), x = _arr[--l], _arr[l] = _arr[j], _arr[j] = x);
         return _arr;
     }
